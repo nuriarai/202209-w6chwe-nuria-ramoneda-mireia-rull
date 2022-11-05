eef775fc437b74653d199bf5d169f38f86bddae8
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-05 10:20:03 +0100

    Add debug setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import "./loadEnvirontment.js";
+import debugCreator from "debug";
 import mongoose from "mongoose";
 import express from "express";
 import chalk from "chalk";
 import { Robot } from "./database/models/Robot.js";
+
+const debug = debugCreator("robots: root");
+
+debug("hola");
 
 const uri = process.env.MONGODB_URL;
 
 try {
   await mongoose.connect(uri);
   const robots = await Robot.find();
-  console.log(robots);
+  debug(robots);
 } catch (error: unknown) {
   console.log("Error on connecting to database", (error as Error).message);
 }
@@ -19,5 +24,7 @@ const port = process.env.PORT;
 const app = express();
 
 const server = app.listen(port, () => {
-  console.log(chalk.yellow(`Server listening on: http://localhost:${port}`));
+  debug(chalk.yellow(`Server listening on: http://localhost:${port}`));
 });
+
+export default server;
